9535a0580bd52e64f18cebd005eee83106f1f891
Tiago Rios da Rocha <tiagoriosrocha@gmail.com>
2024-08-23 07:58:49 -0300

    cabecalhos ok

--- beecrowd/1019.c
+++ beecrowd/1019.c
@@ -1,4 +1,12 @@
-// Resolvido por: Joaquim R. Debiasi e Pedro Molinaro
+/***************************************************************
+    Instituto Federal do Rio Grande do Sul - Câmpus Ibirubá
+    Curso Técnico Integrado em Informática
+    Olimpíada de Programação 2023
+
+    Alunos:
+    - Joaquim R. Debiasi
+    - Pedro Molinaro
+***************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
--- beecrowd/2591-2.py
+++ beecrowd/2591-2.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+# Exercícicio HameKameKa -> Solução otimizada
+# Link: https://judge.beecrowd.com/pt/problems/view/2591
+# Tiago Rios da Rocha
+# 17 de Agosto de 2024
+
 l = int(input())
 v = [
     f"k{'a' * (e.split('k')[0].count('a') * e.split('k')[1].count('a'))}"
